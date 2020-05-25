3c953a90b6a82ed423fee5d4371e3b0b04f1ebc2
lau-eliaschev <62470474+lau-eliaschev@users.noreply.github.com>
2020-05-25 16:34:30 -0300

    Add files via upload

---
+++ HashtagsUsados.js
@@ -0,0 +1,12 @@
+db.tweets_v01.aggregate(
+  [
+    {$project: {"hashtags": 1, _id: 0}},
+    {$unwind: "$hashtags"},
+    {$match: {"hashtags": {$ne: null}}},
+    {$group: {_id: {"hashtags": "$hashtags"}, 
+                    count: {$sum: 1}}},
+    {$sort: {count: -1}},
+    {$project: {"_id": 0, "hashtag": "$_id.hashtags", "count": 1}},
+    {$out: "hashtags_usados"}
+  ]
+)
